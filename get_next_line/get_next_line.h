f59796e11174d39613003119a014edde81116741
Salah Adawi <sadawi@student.hive.fi>
2019-10-25 17:22:34 +0300

    add files

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/25 14:36:39 by sadawi            #+#    #+#             */
+/*   Updated: 2019/10/25 14:48:50 by sadawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+#endif
